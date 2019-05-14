e244d049240d43e3f2cdfc3edceaba3a64bc1853
luisjaramillo6 <luis_jaramillo666@hotmail.com>
2019-05-14 13:49:39 -0500

    crear carpetas

--- administrator/adm_birds/index.php
+++ administrator/adm_birds/index.php
@@ -2,10 +2,7 @@
 	include('../adm_header.php');
 ?>
 <main>
-	<?php
-		$miconexion->consulta("select * from usuarios");
-		$miconexion->verconsulta();
-	?>
+	
 </main>
 <?php
 	include('../adm_footer.php');
