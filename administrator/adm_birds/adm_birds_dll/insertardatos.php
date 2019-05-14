fd40811228f59b07aa291f60aa474595a7f283f3
Kevinq181 <kevin.kjqm@gmail.com>
2019-05-14 15:23:02 -0500

    Insert css

---
+++ administrator/adm_birds/adm_birds_dll/insertardatos.php
@@ -0,0 +1,6 @@
+<?php 
+include("../../security/security.php");
+if (isset($_POST["nombre"], $_POST["nombre_cientifico"], $_POST["habitat"]) and $_POST["categoria"] !="" and $_POST["file"]!="" and $_POST["textarea"]!="" ){
+
+
+}
--- administrator/adm_birds/adm_birds_internas/insert.php
+++ administrator/adm_birds/adm_birds_internas/insert.php
@@ -1,31 +1,55 @@
 <?php
 include('../../adm_header.php');
+include('../adm_birds_dll/insertardatos.php');
 ?>
 <main>
+	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
+	<link href='../adm_birds_css/insert.css' rel='stylesheet' type='text/css' />
+	<form class='form' method="post" action="insertardatos.php">
+		<h2>ESPECIE</h4>
+			<div class="">
+				<div class="form_item col-12 col-lg-5">
+					<input type="name" id="nombre" class="form_input" required>
+					<label for="name" class="form_label">NOMBRE DE ESPECIE</label>
+				</div>
+				<div class="form_item col-12 col-lg-5">
+					<input type="name" id="nombre_cientifico" class="form_input" required>
+					<label for="name" class="form_label">NOMBRE CIENTIFICO</label>
+				</div>
+			</div>
+			<div class="form_item col-12 col-lg-5">
+				<input type="name" id="habitat" class="form_input" required>
+				<label for="name" class="form_label">HABITAT</label>
+			</div>
 
-	<section>
-		<form method="post" name="fomulario1" action="">
-			<div class="datos">
-				<label for="nombre_especie">Nombre de especie: </label>
-				<input type="text" name="nombre_especie" placeholder="Nombre de especie" required="" />
+			<div class="form_item col-12 col-lg-5">
+				<select class="form_select" id="categoria" required>
+					<option class="form_option" value="" disabled selected>SELECCIONAR CATEGORIA</option>
+					<option class="form_option" value="">AVES</option>
+				</select>
+				<label class="form_label">CATEGORIA</label>
 			</div>
-			<div class="datos">
-				<br>
-				<label for="nombre_cientifico">Nombre científico: </label>
-				<input type="text" name="nombre_cientifico" placeholder="Nombre cientifico" required="" />
+
 			</div>
-			<div class="datos">
-				<br>
-				<label for="decripcion">Descripción: </label>
-				<textarea name="textarea" rows="5" cols="85"></textarea>
+
+			<div class="row justify-content-center">
+				<div class="form_item col-11">
+					<input class="form_input form_upload" type="file" id="file" name="file" multiple required>
+					<label class="form_label">ELEGIR IMAGEN</label>
+				</div>
 			</div>
-			<div class="datos">
-				<label for="habitat">Habitat: </label>
-				<input type="text" name="habitat" placeholder="Habitat" required="" />
+
+			<div class="row justify-content-center">
+				<div class="form_item col-11">
+					<textarea class="form_textarea" id="textarea" name="textarea" rows="3" cols="444" required></textarea>
+					<label for="textarea" class="form_label">DESCRIPCIÓN</label>
+				</div>
 			</div>
-			<input type="submit" class="boton" name="guardar" value="   Guardar   " />
-		</form>
-	</section>
+
+			<div class="row justify-content-center">
+				<button class="submit" type="submit">REGISTRAR</button>
+			</div>
+	</form>
 
 </main>
 <?php
