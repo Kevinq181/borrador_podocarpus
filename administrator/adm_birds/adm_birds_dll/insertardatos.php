4abe8fd7fc4a50933ff1821ccd87079dd2d32f6c
Kevinq181 <kevin.kjqm@gmail.com>
2019-05-15 11:04:07 -0500

    Insertar datos con error

--- administrator/adm_birds/adm_birds_dll/insertardatos.php
+++ administrator/adm_birds/adm_birds_dll/insertardatos.php
@@ -1,6 +1,20 @@
-<?php 
+<?php
 include("../../security/security.php");
-if (isset($_POST["nombre"], $_POST["nombre_cientifico"], $_POST["habitat"]) and $_POST["categoria"] !="" and $_POST["file"]!="" and $_POST["textarea"]!="" ){
 
 
+
+if (!$miconexion) {
+    die('No se ha podido conectar a la base de datos');
+}else{
+    echo $_POST['nombre'];
+
+    echo $_POST['nombre_cientifico'];
+    
+    echo $_POST['descripcion'];
+    
+    echo $_POST['habitat'];
 }
+
+$result = mysqli_query("insert into aves(nombre, nombre_c, descripcion,habitat) values ('$_POST[nombre]', '$_POST[nombre_cientifico]', '$_POST[descripcion]', '$_POST[habitat]')", $miconexion);
+
+?>
--- administrator/adm_birds/adm_birds_internas/insert.php
+++ administrator/adm_birds/adm_birds_internas/insert.php
@@ -1,47 +1,37 @@
 <?php
 include('../../adm_header.php');
-include('../adm_birds_dll/insertardatos.php');
+
 ?>
 <main>
 	<link href="https://fonts.googleapis.com/css?family=Baloo+Bhai" rel="stylesheet">
 	<link href='../adm_birds_css/insert.css' rel='stylesheet' type='text/css' />
-	<form class='form' method="post" action="insertardatos.php">
+	<form class='form' method="post" action="../adm_birds_dll/insertardatos.php">
 		<h2>ESPECIE</h2>
 			<div class="">
 				<div class="form_item col-12 col-lg-5">
-					<input type="name" id="nombre" class="form_input" required>
-					<label for="name" class="form_label">NOMBRE DE ESPECIE</label>
+					<input type="nombre" name="nombre" id="nombre" class="form_input" required>
+					<label for="nombre" class="form_label">NOMBRE DE ESPECIE</label>
 				</div>
 				<div class="form_item col-12 col-lg-5">
-					<input type="name" id="nombre_cientifico" class="form_input" required>
-					<label for="name" class="form_label">NOMBRE CIENTIFICO</label>
+					<input type="nombre_cientifico" name="nombre_cientifico" id="nombre_cientifico" class="form_input" required>
+					<label for="nombre_cientifico" class="form_label">NOMBRE CIENTIFICO</label>
 				</div>
 			</div>
 			<div class="form_item col-12 col-lg-5">
-				<input type="name" id="habitat" class="form_input" required>
-				<label for="name" class="form_label">HABITAT</label>
-			</div>
-
-			<div class="form_item col-12 col-lg-5">
-				<select class="form_select" id="categoria" required>
-					<option class="form_option" value="" disabled selected>SELECCIONAR CATEGORIA</option>
-					<option class="form_option" value="">AVES</option>
-				</select>
-				<label class="form_label">CATEGORIA</label>
-			</div>
-
+				<input type="habitat" name="habitat" id="habitat" class="form_input" required>
+				<label for="habitat" class="form_label">HABITAT</label>
 			</div>
 
 			<div class="row justify-content-center">
 				<div class="form_item col-11">
-					<input class="form_input form_upload" type="file" id="file" name="file" multiple required>
+					<input class="form_input form_upload" type="file" id="file" name="file" multiple>
 					<label class="form_label">ELEGIR IMAGEN</label>
 				</div>
 			</div>
 
 			<div class="row justify-content-center">
 				<div class="form_item col-11">
-					<textarea class="form_textarea" id="textarea" name="textarea" rows="3" cols="444" required></textarea>
+					<textarea class="form_textarea" id="textarea" name="descripcion" rows="3" cols="444" required></textarea>
 					<label for="textarea" class="form_label">DESCRIPCIÓN</label>
 				</div>
 			</div>
